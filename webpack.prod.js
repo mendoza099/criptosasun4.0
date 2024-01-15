5b0f64b13c5563d2b38d7b68ffb6c8abc9a71d22
mendoza099 <sergiomendozadominguez@gmail.com>
2024-01-15 21:19:59 +0000

    final10 final

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,19 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: './'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"), // Asegura que la salida sea en el directorio 'public'
+    publicPath: "./",
+  },
+  plugins: [
+    new Dotenv({
+      safe: true,
+      systemvars: true,
+    }),
+  ],
 });
